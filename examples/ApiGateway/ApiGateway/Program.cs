5f01c1c3fc6eb57b6672389e0245738005277687
drsh <41671372+drawmoon@users.noreply.github.com>
2021-12-14 14:21:03 +0800

    继续简化项目结构与代码，使示例项目更易读

--- examples/ApiGateway/ApiGateway/Program.cs
+++ examples/ApiGateway/ApiGateway/Program.cs
@@ -1,30 +1,18 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
-namespace ApiGateway
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+var builder = WebApplication.CreateBuilder(args);
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration(configure =>
-                {
-                    configure.AddJsonFile("ocelot.json");
-                })
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-}
+builder.Configuration.AddJsonFile("ocelot.json");
+
+// Add services to the container.
+
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseOcelot().Wait();
+
+app.Run();
